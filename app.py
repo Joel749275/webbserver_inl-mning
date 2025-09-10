8343381e812f4d18582956d576570db787d56dfa
Joel749275 <joel.hogg@skola.taby.se>
2025-09-10 14:44:19 +0200

    grund klar

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# test
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def product():
+    contacts = [
+        {'name': 'Joel', 'email': 'joel.hogg@skola.taby.se'},
+    ]
+    return render_template('contact.html', contacts=contacts)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
